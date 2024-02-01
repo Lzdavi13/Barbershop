a3b5119a99e240729dfe55de1e181c1859bfc63b
Luiz D O Santos <lzdavi1325@gmail.com>
2024-02-01 17:38:04 -0300

    feat: add infos in BarbershopInfo component

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Button } from "@/app/_components/ui/button";
 import { Barbershop } from "@prisma/client";
-import { ChevronLeftIcon, MenuIcon } from "lucide-react";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
@@ -45,6 +45,20 @@ function BarbershopInfo({ barbershop }: BarbershopInfoProps) {
           }}
           className="opacity-75"
         />
+      </div>
+
+      <div className="px-5 pt-3 pb-6 border-b solid border-secondary">
+        <h2 className="text-xl font-bold">{barbershop.name}</h2>
+
+        <div className="flex items-center gap-2 mt-2">
+          <MapPinIcon size={18} className="text-primary" />
+          <p className="text-sm">{barbershop.address}</p>
+        </div>
+
+        <div className="flex items-center gap-2 mt-2">
+          <StarIcon size={18} className="text-primary" />
+          <p className="text-sm">5,0 (889 avaliações)</p>
+        </div>
       </div>
     </div>
   );
