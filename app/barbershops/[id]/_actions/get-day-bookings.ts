487194927cf4566cc40b0b7ffd52adc5a2403114
Luiz D O Santos <lzdavi1325@gmail.com>
2024-02-02 20:43:42 -0300

    chore: bug database

--- app/barbershops/[id]/_actions/get-day-bookings.ts
+++ app/barbershops/[id]/_actions/get-day-bookings.ts
@@ -3,10 +3,10 @@
 import { db } from "@/app/_lib/prisma";
 import { endOfDay, startOfDay } from "date-fns";
 
-export const getDayBookings = async (babershopId: string, date: Date) => {
+export const getDayBookings = async (barbershopId: string, date: Date) => {
   const bookings = await db.booking.findMany({
     where: {
-      babershopId,
+      barbershopId,
       date: {
         lte: endOfDay(date),
         gte: startOfDay(date),
