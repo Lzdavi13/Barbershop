84b735d7be2e66b91166166e30162cdda4a38136
Luiz D O Santos <lzdavi1325@gmail.com>
2024-02-05 18:04:02 -0300

    feat: ensure semantic tags are used on footer and header

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,12 +1,10 @@
 function Footer() {
   return (
-    <div className="flex py-6 px-5 w-full bg-secondary">
-      <div>
-        <h3 className="text-xs text-gray-400 opacity-75">
-          © 2023 Copyright <span className="font-bold">FSW Barber</span>
-        </h3>
-      </div>
-    </div>
+    <footer className="flex py-6 px-5 w-full bg-secondary">
+      <h3 className="text-xs text-gray-400 opacity-75">
+        © 2023 Copyright <span className="font-bold">FSW Barber</span>
+      </h3>
+    </footer>
   );
 }
 
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -8,24 +8,26 @@ import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 
 function Header() {
   return (
-    <Card>
-      <CardContent className="flex justify-between p-5 items-center">
-        <Link href="/">
-          <Image src="/Logo.png" alt="fsw barber" height={18} width={120} />
-        </Link>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline" size="icon">
-              <MenuIcon size={16} />
-            </Button>
-          </SheetTrigger>
+    <header>
+      <Card>
+        <CardContent className="flex justify-between p-5 items-center">
+          <Link href="/">
+            <Image src="/Logo.png" alt="fsw barber" height={18} width={120} />
+          </Link>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon">
+                <MenuIcon size={16} />
+              </Button>
+            </SheetTrigger>
 
-          <SheetContent className="p-0">
-            <SideMenu />
-          </SheetContent>
-        </Sheet>
-      </CardContent>
-    </Card>
+            <SheetContent className="p-0">
+              <SideMenu />
+            </SheetContent>
+          </Sheet>
+        </CardContent>
+      </Card>
+    </header>
   );
 }
 
